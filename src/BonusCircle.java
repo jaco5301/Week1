4dde18b4f4262db9e6710208b3b40c644bb1f6e9
Jaco5301 <Jaco5301@ISS147D8-25.scdsb.pri>
2015-09-11 10:32:35 -0400

    Inital commit

--- src/BonusCircle.java
+++ src/BonusCircle.java
@@ -3,9 +3,9 @@ public class BonusCircle {
     public static void main(String[] args) {
         SketchPadWindow win = new SketchPadWindow(600, 400);
         Pen p = new WiggleRainbowPen(win);
-        p.setWidth(10);
-        int size=10;
-        int mover=7;
+        p.setWidth(1);
+        int size=1;
+        int mover=1;
         for(int count=0; count<360; count++)
         {
             p.move(size);
--- src/Momentum.java
+++ src/Momentum.java
@@ -1,20 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Jaco5301
- */
+import hsa.*;
 public class Momentum {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
