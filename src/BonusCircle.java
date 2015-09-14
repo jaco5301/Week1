412877ee453a331925a463cad9642c3e1c255103
Jaco5301 <Jaco5301@ISS147D8-25.scdsb.pri>
2015-09-14 10:12:31 -0400

    Inital commit

--- src/BonusCircle.java
+++ src/BonusCircle.java
@@ -3,18 +3,14 @@ public class BonusCircle {
     public static void main(String[] args) {
         SketchPadWindow win = new SketchPadWindow(600, 400);
         Pen p = new WiggleRainbowPen(win);
-        p.setWidth(1);
+        p.setWidth(5);
         int size=1;
         int mover=1;
         for(int count=0; count<360; count++)
         {
-            p.move(size);
+        p.move(size);
         p.turn(mover);
         }
-        
-        //move over and draw another square bb
-   
-        
     }
     
 }
