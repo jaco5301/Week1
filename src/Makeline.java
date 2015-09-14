8fcfc1f9f13dde5ee67cf01a71159d01aed68803
Jaco5301 <Jaco5301@ISS147D8-25.scdsb.pri>
2015-09-14 09:54:47 -0400

    Inital commit

--- src/Makeline.java
+++ src/Makeline.java
@@ -1,14 +1,25 @@
 import TurtleGraphics.*;
+import hsa.*;
 public class Makeline {
     public static void main(String[] args) {
         SketchPadWindow s = new SketchPadWindow(400, 400);
         StandardPen p =new StandardPen(s);
+        Console c = new Console();
+        int x1, x2, y1, y2;
+        c.print("Enter X1: ");
+        x1 = c.readInt();
+        c.print("Enter Y1: ");
+        y1 = c.readInt();
+        c.print("Enter X2: ");
+        x2 = c.readInt();
+        c.print("Enter Y2: ");
+        y2 = c.readInt();
         p.up();
-        p.move(100-200,50-200);
+        p.move(x1-200,y1-200);
         p.down();
-        p.drawString("(" + 100 + ". " + 50 + ")");
-        p.move(300-200,300-200);
-        p.drawString("(" + 300 + ", " + 300 + ")");
+        p.drawString("(" + y1 + ", " + x1 + ")");
+        p.move(y1-200,y2-200);
+        p.drawString("(" + y2 + ", " + x1 + ")");
         
     }
     
