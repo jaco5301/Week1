01f09f22d49a8a3df31d9b5732a74e1c8b58dd52
Jaco5301 <Jaco5301@ISS147D8-25.scdsb.pri>
2015-09-14 10:39:18 -0400

    stuff

--- src/Pizzacost.java
+++ src/Pizzacost.java
@@ -1,20 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Jaco5301
- */
+import hsa.*;
 public class Pizzacost {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Console c = new Console();
+        double size, topnum, base, toppings, base2, total;
+        base = 2.25;
+        toppings= 0.75;
+        base2= 0.5;
+        total= 0;
+        c.print("What size of pizza to do you want (in inches): ");
+        size=c.readDouble();
+        c.print("\nHow many toppings do you want? ");
+        topnum=c.readDouble();
+        total= base + (toppings * topnum) + (size * base2);
+        c.print("Your total cost for your pizza is: $" + total);
     }
     
 }
