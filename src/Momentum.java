305b4f00fe54d7aeef300893d731355b9d85f3dd
Jaco5301 <Jaco5301@ISS147D8-25.scdsb.pri>
2015-09-14 09:31:09 -0400

    Inital commit

--- src/Momentum.java
+++ src/Momentum.java
@@ -1,7 +1,16 @@
 import hsa.*;
 public class Momentum {
     public static void main(String[] args) {
-        
+        Console c = new Console();
+        int mass;
+        int velocity;
+        int momentumE;
+        c.print("Enter object's mass: ");
+        mass = c.readDouble();
+        c.print("Enter the object's velocity: ");
+        velocity=c.readDouble();
+        momentumE = mass * velocity;
+         c.print("The momentum is " + momentumE);
     }
     
 }
